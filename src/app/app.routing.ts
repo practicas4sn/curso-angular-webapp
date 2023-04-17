5488e0268b0011e5c542ccf002e5a615230dbc1a
practicas4sn <practicas4@servinet.net>
2023-04-17 14:18:13 +0200

    Segundo commit incorporo componente productos

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from "@angular/router";
 // componentes
 import { HomeComponent } from './components/home.component';
 import { ErrorComponent } from './components/error.component';
+import { ProductosListoComponent } from './components/productos-list.component';
 
 const appRoutes: Routes = [
     {path: "", component: HomeComponent},
     {path: "home", component: HomeComponent},
+    {path: "productos", component:ProductosListoComponent},
     {path: "**", component: ErrorComponent}
 ];
 
